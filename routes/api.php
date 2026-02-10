2dae7bf49c3dbbb8e96a39485aa1007be97fc979
midecal-hub <medical.hub.dev@gmail.com>
2026-02-10 17:05:38 +0200

    edit routes

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,10 @@ use App\Http\Controllers\Webhooks\OmnifulReturnOrderWebhookController;
 use App\Http\Controllers\Webhooks\OmnifulStockTransferWebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('webhooks/omniful/order', OmnifulOrderWebhookController::class);
-Route::post('webhooks/omniful/return-order', OmnifulReturnOrderWebhookController::class);
-Route::post('webhooks/omniful/purchase-order', OmnifulPurchaseOrderWebhookController::class);
-Route::post('webhooks/omniful/product', OmnifulProductWebhookController::class);
-Route::post('webhooks/omniful/inwarding', OmnifulInwardingWebhookController::class);
-Route::post('webhooks/omniful/inventory', OmnifulInventoryWebhookController::class);
-Route::post('webhooks/omniful/stock-transfer', OmnifulStockTransferWebhookController::class);
+Route::post('webhooks/omniful/order/whk_7d4c91b2f6e84a9bb3d12c7e', OmnifulOrderWebhookController::class);
+Route::post('webhooks/omniful/return-order/whk_3a6e2d9f4c7841b8a5d0e33f', OmnifulReturnOrderWebhookController::class);
+Route::post('webhooks/omniful/purchase-order/whk_8c1f5e27b94d4a6f9e2c301d', OmnifulPurchaseOrderWebhookController::class);
+Route::post('webhooks/omniful/product/whk_5e2a7c19d8434fb6a0c21d9e', OmnifulProductWebhookController::class);
+Route::post('webhooks/omniful/inwarding/whk_1f9b3d6e24c8475aa2e0b91c', OmnifulInwardingWebhookController::class);
+Route::post('webhooks/omniful/inventory/whk_4b8e1c7d29f34a6ab5d203ef', OmnifulInventoryWebhookController::class);
+Route::post('webhooks/omniful/stock-transfer/whk_9a2d4f1c6b834e7da0c35b8f', OmnifulStockTransferWebhookController::class);
